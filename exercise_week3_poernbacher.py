65df904598c289d3854ad9ee217a985f5df412d5
sandrapoer <127725170+sandrapoer@users.noreply.github.com>
2023-03-28 14:05:12 +0200

    Update exercise_week3_poernbacher.py

--- exercise_week3_poernbacher.py
+++ exercise_week3_poernbacher.py
@@ -15,8 +15,47 @@ def hamming(text1, text2):
     return hamming_distance
 
 # QUESTION NR 3
-class Vehicle()
+class Vehicle():
+    def __init__(self, color, fuel_type):
+        self.color = color
+        self.fuel_type = fuel_type
+
+    def doors(self, doors):
+        self.doors = doors
+
+    def __str__(self):
+        return f"Color: {color}, Fuel Type: {fuel_type}, Doors: {doors}"
+
+class Bus(Vehicle):
+    def passengers(self, passengers):
+        self.passengers = passengers
+
+    def __str__(self):
+        f"Color: {color}, Fuel Type: {fuel_type}, Doors: {doors}, Passengers: {passengers}"
+
 
 # QUESTION NR 4
+class Book():
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+    def __str__(self):
+        return print(f"{name}, {author}")
 
 # QUESTION NR 5
+class BookShelf():
+    def __init__(self):
+        pass
+
+    def add_book_list(self, books):
+        self.books = books
+
+    def books_by_author(self, author):
+        self.author = author
+        return
+
+    def get_books(self):
+        return
+
+    def clear_shelf(self):
