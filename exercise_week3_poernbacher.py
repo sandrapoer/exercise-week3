f51437e5035dbc2a359e7b34d0a73f4376dd7114
sandrapoer <127725170+sandrapoer@users.noreply.github.com>
2023-03-28 13:40:56 +0200

    Update exercise_week3_poernbacher.py

---
+++ exercise_week3_poernbacher.py
@@ -0,0 +1,14 @@
+# QUESTION NR 1
+def count_vowels(text):
+    if type(text) == int:
+        return 42
+    else:
+        return len(text)
+
+# QUESTION NR 2
+
+# QUESTION NR 3
+
+# QUESTION NR 4
+
+# QUESTION NR 5
