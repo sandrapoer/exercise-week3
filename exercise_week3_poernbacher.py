b38e7185564cf37a17e5e1530e3b409ec2de127f
sandrapoer <127725170+sandrapoer@users.noreply.github.com>
2023-03-28 13:53:47 +0200

    Update exercise_week3_poernbacher.py

--- exercise_week3_poernbacher.py
+++ exercise_week3_poernbacher.py
@@ -6,8 +6,16 @@ def count_vowels(text):
         return len(text)
 
 # QUESTION NR 2
+def hamming(text1, text2):
+    if len(text1) not len(text2):
+        return 0
+
+    hamming_distance = 0
+
+    return hamming_distance
 
 # QUESTION NR 3
+class Vehicle()
 
 # QUESTION NR 4
 
